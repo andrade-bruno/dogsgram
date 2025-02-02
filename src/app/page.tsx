1e9be3d193af0311a223613945e2623bbe0dbf4b
Bruno Andrade <brunoandrade.contato@icloud.com>
2025-02-01 22:11:52 -0300

    feat: init vue migration to next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      <main>
-        <h1> Dogs Next</h1>
-      </main>
-    </div>
+    <main>
+      <h1 className="title">Dogs Next</h1>
+    </main>
   );
 }
